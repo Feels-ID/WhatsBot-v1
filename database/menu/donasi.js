21259efe2bd5879289a17f2b408396d70c4a2b89
AGUSZ PAKEZ <78070315+axfcap@users.noreply.github.com>
2021-02-01 20:36:47 +0700

    Add files via upload

---
+++ database/menu/donasi.js
@@ -0,0 +1,13 @@
+const donasi = (prefix, botName, ownerName) => {
+	return `
+┏ *〈 ${botName} 〉* 
+
+
+
+◪ *DONASI*
+  │
+  ├─ ❏ 0896-9435-4384
+  └─ ❏ 0896-9638-9730
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.donasi = donasi
---
+++ database/menu/info.js
@@ -0,0 +1,15 @@
+const info = (prefix, botName, ownerName) => {
+	return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.4
+   ┠≽ *YouTube* :https://youtube.com/channel/UCKP-E8RwFkJKhe-9uz0s9RQ
+   ┠≽ *Instagram* :https://Instagram.com/axfc_ap
+   
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.info = info
