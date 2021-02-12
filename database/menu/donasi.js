98e1ea0caa14eff454dec50e4e6ece93e22fff70
AGUSZ PAKEZ <78070315+axfcap@users.noreply.github.com>
2021-02-12 15:04:17 +0700

    Add files via upload

---
+++ database/menu/donasi.js
@@ -0,0 +1,13 @@
+const donasi = (prefix, botName, ownerName) => {
+	return `
+┏ *〈 DONASI NYA OMM 〉*
+┠──────────────╼
+┠≽ *〈 OVO 〉*
+┃💎 *0896-9435-4384*
+┠──────────────╼
+┠≽ *〈 PULSA 〉*
+┃💎 *0896-9435-4384* 
+┠──────────────╼
+  ⚕️⚕️ *Developer © ${botName} ⚕️⚕️`
+}
+exports.donasi = donasi
